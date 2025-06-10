ec23de5ce80cf0b170d9c1f1b14071bd5fda4a6d
Students <irfas.mec@gmail.com>
2025-06-10 18:08:34 +0530

    Product Individual page completed

---
+++ src/pages/ProductCategory.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
+import { useAppContext } from '../context/appcontext';
+
+const ProductCategory = () => {
+    const { Products } = useAppContext();
+    const { category } = useParams();
+    
+    const filteredProducts = Products.filter(
+        (product) => product.category.toLowerCase() === category.toLowerCase()
+    );
+
+    return (
+        <div className='mt-16'>
+            <div className='flex flex-col items-end w-max'>
+                <p className='text-2xl font-medium'>
+                    {category.toUpperCase()}
+                </p>
+                <div className='w-16 h-0.5 bg-primary rounded-full'> </div>
+            </div>
+            {filteredProducts.length > 0 ? (
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6 mt-6'>
+                    {filteredProducts.map((product) => (
+                        <ProductCard key={product._id} product={product} />
+                    ))}
+                </div>
+            ) : (
+                <div>No products found</div>
+            )}
+        </div>
+    );
+}
+
+export default ProductCategory
